865477e3149349da40e2e82789b022b9fd9a6d0f
akkessatsu <raulbragauchoarz@gmail.com>
2023-06-02 10:08:28 -0300

    Refatorando código e adicionando documentação

--- app.py
+++ app.py
@@ -1,53 +1,90 @@
 class App():
     def __init__(self):
+        """Generates a title and a header for information at 
+        the instance of the class.
+        """
+        
         App.title("The shape of us!")
         print()
         print("=> Informe alguns dados para começar: ")
         print()
-        App.generateHeader()
+        App.generate_header()
 
     @classmethod
     def padding(cls):
+        """Generates two linebreaks
+        """
         print()
         print()
 
     @classmethod
-    def generateHeader(cls):
+    def generate_header(cls):
+        """Generates a header that indicates some information about
+        level of activity.
+        """
         print("OBS: O Nivel de atividade varia de 1 (Sedentário) a 4 (Muito Ativo) !")
-        print("Ex: {:^8s} {:^22s} {:^14s} {:^20s} {:^10s} ".format("1.70", "70.0", "M", "3", "20"))
+        print(f"Ex: {'1.70':^8s} {'70.0':^22s} {'M':^14s} {'3':^20s} {'20':^10s} ")
         print()
 
     @classmethod
+        
     def row(cls):
+        """Generates a row of 81 asterisks.
+        """
         print('*' * 81)
 
     @classmethod
-    def rowTable(cls):
+    def row_table(cls):
+        """Generates a table row in the following format:
+        '+-------------------------++-------------------------++-------------------------+'
+        """
         print(f"+{'-' * 25}++{'-' * 25}++{'-' * 25}+")
 
     @classmethod
-    def title(cls, title):
+    def title(cls, title: str):
+        """Generates a formatted title.
+
+        Args:
+            title: A string containing the title to be formatted.
+        """
         App.row()
-        print('*{:^79s}*'.format(title))
+        print(f'*{title:^79s}*')
         App.row()
 
     @classmethod
-    def collectUserData(cls):
-        print("{:^16s}".format("Altura (m):"), end="")
-        print("{:^18s}".format("Peso (Kg):"), end="")
-        print("{:^18s}".format("Sexo (M/F):"), end="")
-        print("{:^18s}".format("Nvl de Ativ:"), end="")
-        print("{:^16s}".format("Idade :"))
+    def collect_user_data(cls) -> list:
+        """Collect user information and returns it
 
-        userData = input("")
-        userData = userData.split(" ")
+        Returns:
+            Returns a list containing the user data passed 
+            to the function (Height, Mass, Sex, Lvl of Activity, Age)
+        """
+        print(f"{'Altura (m):':^16s}", end="")
+        print(f"{'Peso (Kg):':^18s}", end="")
+        print(f"{'Sexo (M/F):':^18s}", end="")
+        print(f"{'Nvl de Ativ:':^18s}", end="")
+        print(f"{'Idade :':^16s}")
+
+        user_data = input("")
+        user_data = user_data.split(" ")
         print()
         App.row()
 
-        return userData
+        return user_data
 
     @classmethod
-    def listUserData(cls, values):
+    def list_user_data(cls, values: list[str]) -> list[str|float]:
+        """Returns the user data as a list with its elements converted.
+
+        Args:
+            values: 
+                The collected user data obtained by collect_user_data
+                function.
+
+        Returns:
+            A list containing the elements of collect_user_data as a float
+            if it is not a empty value or a string value.
+        """
         list = []
         for i in values:
             if i != "":
@@ -58,53 +95,86 @@ class App():
         return list
 
     @classmethod
-    def validateData(cls, values):
+    def generate_dict(cls, list: list[str|float]) -> dict[str:float|str]:
+        """Generates a dictionary given a list of values.
+        
+        Args
+            list: 
+                The collected user data obtained as a list of string and float elements
+        Returns:
+            Generates a dictionary based on the list of user data passed as argument.
+        """
+        user_information = [
+                'altura',
+                'peso',
+                'sexo',
+                'nvlAtiv',
+                'idade'
+               ]
+        dic = {
+            elm: list[cont] for cont, elm in enumerate(user_information)
+        }
+        return dic
+
+    @classmethod
+    def validate_data(cls, values:list[str]) -> list[str|float]:
+        """Returns the collected user data if it is valid.
+        
+        Args:
+            values: A list of strings obtained by the collect_user_data function.
+
+        Returns:
+            Returns the result of the list_user_data function
+            if all fields of the data are collected and there are not any data type mismatch
+            between the input of the user and the required data.
+        """
         while True:
             try:
-                list = App.listUserData(values)
-                userData = App.generateDict(list)
+                list = App.list_user_data(values)
+                user_data = App.generate_dict(list)
 
             except IndexError:
                 print()
                 print('Preencha todos os dados para prosseguir!'.upper())
                 print()
-                App.generateHeader()
-                values = App.collectUserData()
+                App.generate_header()
+                values = App.collect_user_data()
 
             except ValueError:
                 print()
                 print('Valor inválido!'.upper())
                 print()
-                App.generateHeader()
-                values = App.collectUserData()
+                App.generate_header()
+                values = App.collect_user_data()
 
             else:
-                list = App.listUserData(values)
+                list = App.list_user_data(values)
                 break
 
         return list
 
     @classmethod
-    def generateDict(cls, list):
-        dic = {'altura': None, 'peso': None, 'sexo': None, 'nvlAtiv': None, 'idade': None}
-        cont = 0
-        for k, v in dic.items():
-            dic[k] = list[cont]
-            cont += 1
+    def print_result(cls, list: list[str]):
+        """Generates a printed message containing the result
+        of TMB, IMC o QtdCal
 
-        return dic
-
-    @classmethod
-    def printresult(cls, list):
+        Args:
+            list (list[str]): The result of user TMB, BMI or QtdCal
+        """
         print()
         App.row()
-        print('|{:^25s}||{:^25s}||{:^25s}|'.format(str(list[0][0]), str(list[0][1]),
-                                                   str(list[0][2])))
+        print(f"|{str(list[0][0]):^25s}||{str(list[0][1]):^25s}||{str(list[0][2]):^25s}|")
         App.row()
 
     @classmethod
     # (imc, status)
-    def creatTableImc(cls, imc, status):
+    def create_table_imc(cls, imc: float, status: str):
+        """Generates a table containing information about BMI and the user BMI
+
+        Args:
+            imc: The value of the user BMI
+            status: A message that indicates in which status the user fits
+        """
         content = [['Tabela de IMC', 'Intervalo', ' Status'],
                    ['Menos do que: ', '18,5', 'Abaixo do Peso !'],
                    ['Entre: ', '18,5 e 24,9', 'Peso Normal!'],
@@ -117,81 +187,129 @@ class App():
         # analysingImc -> status
         result = [['SEU IMC: ', str(imc), status]]
         print()
+        
         for row in range(0, len(content)):
-            App.rowTable()
-            print('|{:^25s}||{:^25s}||{:^25s}|'.format(content[row][0], content[row][1],
-                                                       content[row][2]))
+            App.row_table()
+            print(f"|{content[row][0]:^25s}||{content[row][1]:^25s}||{content[row][2]:^25s}|")
+            
             if row == 6:
-                App.rowTable()
-                App.printresult(result)
+                App.row_table()
+                App.print_result(result)
 
     @classmethod
-    def creatTableQtdCal(cls, dict):
+    def create_table_qtd_cal(cls, dict: dict[str:float|str]):
+        """Generates a table containing information about quantity of calories
+
+        Args:
+            dict: 
+                A dictionary that contains the names of nutrients as a key (carbohydrates, proteins, fats)
+                and the amount of calories in each one as values.
+                See generate_nutrients at server.py for more information about it
+        """
+        carboidratos_value = round(float((dict["carboidratos"])) / 4.0)
+        proteinas_value    = round(float((dict["proteinas"])) / 4.0)
+        gorduras_value     = round(float((dict["gorduras"])) / 9.0)
+        
         content = [
-            ["Carboidratos: ", dict["carboidratos"], round(float((dict["carboidratos"])) / 4.0, 2)],
-            ["Proteínas: ", dict["proteinas"], round(float((dict["proteinas"])) / 4.0, 2)],
-            ["Gorduras", dict["gorduras"], round(float((dict["gorduras"])) / 9.0, 2)]
+            ["Carboidratos: ", dict["carboidratos"], carboidratos_value, 2],
+            ["Proteínas: ",    dict["proteinas"],    proteinas_value, 2],
+            ["Gorduras",       dict["gorduras"],     gorduras_value, 2]
         ]
 
         for row in range(0, len(content)):
-            App.rowTable()
-            print('|{:^25}||{:^25}||{:^25}|'.format(str(content[row][0]), str(content[row][1]) + " kcal",
+            App.row_table()
+            print('|{:^25}||{:^25}||{:^25}|'.format(str(content[row][0]),
+                                                    str(content[row][1]) + " kcal",
                                                     str(content[row][2]) + " g"))
-            App.rowTable()
+            App.row_table()
 
     @classmethod
-    def menu(cls, response):
+    def menu(cls, response: dict[str: float|dict[str: float]|str]):
+        """Generates the main menu of the application
+
+        Args:
+            response: 
+                A dictionary that contains all the user information('altura',
+                'peso',
+                'sexo',
+                'nvlAtiv',
+                'idade',
+                'imc',
+                'statusImc',
+                'tmb',
+                'calorias',
+                'nutrientes'
+                )
+                collected and generated by the functions of the application.
+        """
         while True:
             App.padding()
             print("=> Selecione uma opção: ")
             print()
-            print('{:^16s}{:^18s}{:^18s}{:^18s}{:2s}'.format("1 - IMC", "2 - TMB", "3 -  QTD KCAL", "4 - SAIR", ""),
-                  end="\t")
+            print(f'{"1 - IMC":^16s}{"2 - TMB":^18s}{"3 -  QTD KCAL":^18s}{"4 - SAIR":^18s}{"":2s}', end="\t")
             opt = input()
             App.padding()
 
-            if opt == "1":
-                App.title("IMC")
-                print()
-                print("{:^81s}".format("O Indice de Massa Corporal (IMC) é um parâmetro"))
-                print("{:^81s}".format("utilizado para saber se o peso está de acordo com a altura de um"))
-                print(
-                    "{:^81s}".format("indivíduo, o que pode interferir diretamente na sua saúde e qualidade de vida!"))
-                App.creatTableImc(response["imc"], response["statusImc"])
+            match opt:
+                case '1':
+                    App.title("IMC")
+                    print()
+                    print("{:^81s}".format(
+                        "O Indice de Massa Corporal (IMC) é um parâmetro"))
+                    
+                    print("{:^81s}".format(
+                        "utilizado para saber se o peso está de acordo com a altura de um"))
+                    
+                    print("{:^81s}".format(
+                        "indivíduo, o que pode interferir diretamente na sua saúde e qualidade de vida!"))
+                    
+                    App.create_table_imc(response["imc"], response["statusImc"])
+                    
+                case '2':
+                    App.title("Taxa Metabólica Basal: ")
+                    print()
+                    print("{:^81s}".format(
+                        "A Taxa de Metabolismo Basal (TMB) é a quantidade"))
+                    
+                    print("{:^81s}".format(
+                        "mínima de energia (calorias) necessária para manter as"))
+                    
+                    print("{:^81s}".format(
+                        "funções vitais do organismo em repouso. Essa taxa pode variar"))
+                    
+                    print("{:^81s}".format(
+                        "de acordo com o sexo, peso, altura, idade e nível de atividade física."))
 
-            elif opt == "2":
-                App.title("Taxa Metabólica Basal: ")
-                print()
-                print("{:^81s}".format("A Taxa de Metabolismo Basal (TMB) é a quantidade"))
-                print("{:^81s}".format("mínima de energia (calorias) necessária para manter as"))
-                print("{:^81s}".format("funções vitais do organismo em repouso. Essa taxa pode variar"))
-                print("{:^81s}".format("de acordo com o sexo, peso, altura, idade e nível de atividade física."))
+                    result = [['RESULTADO :', 'SUA TMB:', str(response['tmb']) + " kcal"]]
+                    App.print_result(result)
+                    
+                case '3':
+                    nut = response["nutrientes"]
+                    App.title("Quantidade de Calorias: ")
+                    print()
+                    print("{:^81s}".format(
+                        "Calorias são a quantidade de energia que um determinado alimento"))
+                    
+                    print("{:^81s}".format(
+                        "fornece após ser consumido, contribuindo para as funções essenciais do"))
+                    
+                    print("{:^81s}".format(
+                        "organismo, como respiração, produção de hormônios, e funcionamento do cérebro."))
 
-                result = [['RESULTADO :', 'SUA TMB:', str(response['tmb']) + " kcal"]]
-                App.printresult(result)
+                    print()
+                    print("{:^81s}".format(
+                        "Você deve consumir aproximadamente: "))
+                    print()
+                    App.create_table_qtd_cal(nut)
 
-            elif opt == "3":
-                nut = response["nutrientes"]
-                App.title("Quantidade de Calorias: ")
-                print()
-                print("{:^81s}".format("Calorias são a quantidade de energia que um determinado alimento"))
-                print("{:^81s}".format("fornece após ser consumido, contribuindo para as funções essenciais do"))
-                print(
-                    "{:^81s}".format("organismo, como respiração, produção de hormônios, e funcionamento do cérebro."))
-
-                print()
-                print("{:^81s}".format("Você deve consumir aproximadamente: "))
-                print()
-                App.creatTableQtdCal(nut)
-
-                result = [['RESULTADO :', 'SUA QTD DE KCAL:', str(response['cal']) + " kcal"]]
-                App.printresult(result)
-
-            elif opt == "4":
-                print('{:^79s}'.format("Obrigado por usar nosso App !"))
-                App.padding()
-                App.row()
-                break
-
-            else:
-                print("Erro: Opção Inválida!")
+                    result = [['RESULTADO :', 'SUA QTD DE KCAL:', str(response['cal']) + " kcal"]]
+                    App.print_result(result)
+                    
+                case '4':
+                    print('{:^79s}'.format("Obrigado por usar nosso App !"))
+                    App.padding()
+                    App.row()
+                    break
+                
+                case _:
+                    print("Erro: Opção Inválida!")
--- server.py
+++ server.py
@@ -4,43 +4,57 @@ import json
 
 # create a socket object
 print('ECHO SERVER para cálculo do IMC')
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # get a local machine name
 host = '127.0.0.1'
 port = 9999
 
 # bind to the port
-serverSocket.bind((host, port))
+server_socket.bind((host, port))
 
 #start listening requests
-serverSocket.listen()
-print('Serviço rodando na porta {}.'.format(port))
+server_socket.listen()
+print(f'Serviço rodando na porta {port}.')
 
 while True:
     # establish a connection
-    clientSocket, addr = serverSocket.accept()
-    print('Conectado a {}'.format(str(addr)))
+    client_socket, addr = server_socket.accept()
+    print(f'Conectado a {str(addr)}')
 
-    # recive client data
-    received = clientSocket.recv(1024).decode()
+    # receive client data
+    received = client_socket.recv(1024).decode()
 
-    print('Os dados recebidos do cliente são: {}'.format(received))
+    print(f'Os dados recebidos do cliente são: {received}')
 
     # server processing
     received = json.loads(received)
 
     # IMC (Indice de Massa Corporal)
-    def generateImc(dict):
+    def generate_imc(dict: dict[str:float|str]) -> float:
+        """Computes and returns the user BMI.
+
+        Args:
+            dict: A dictionary that contains the user data collected and passed
+            by the functions: validate_data and generate_dict. 
+
+        Returns:
+            A float value that represents the user BMI.
+        """
         h = dict['altura']
         p = dict['peso']
         return round(float(p / (h * h)), 2)
 
     # adding the imc to data sent by the user
-    received['imc'] = generateImc(received)
+    received['imc'] = generate_imc(received)
 
     # Status IMC
-    def analyseImc(imc):
+    def analyse_imc(imc: float) -> str:
+        """Returns a message based in the argument.
+
+        Args:
+            imc: A value that represents the user BMI.
+        """
         if imc > 0 and imc < 18.5:
             status = "Abaixo do Peso!"
         elif imc <= 24.9:
@@ -58,58 +72,88 @@ while True:
         return status
 
     # adding the status of the imc to data sent by the user
-    received['statusImc'] = analyseImc(received['imc'])
+    received['statusImc'] = analyse_imc(received['imc'])
 
     # TMB (Taxa Metabólica Basal)
-    def generateTMB(dict):
+    def generate_TMB(dict: dict[str:float|str]) -> float:
+        """Computes and returns the user TMB.
+
+        Args:
+            dict: A dictionary that contains the user data collected and passed
+            by the functions: validate_data and generate_dict. 
+
+        Returns:
+            A float value that represents the user TMB.
+        """
         sex = dict['sexo']
 
         if sex in 'Mm':
             tmb = 5 + (10 * dict['peso']) + (6.25 * (dict['altura'] * 100)) - (5 * dict['idade'])
-
         else:
             tmb = (10 * dict['peso']) + (6.25 * (dict['altura'] * 100)) - (5 * dict['idade']) - 5
 
         return tmb
 
     # adding the tmb to data sent by the user
-    received['tmb'] = generateTMB(received)
+    received['tmb'] = generate_TMB(received)
 
-    def generateCal(dict):
-        if dict['nvlAtiv'] == 1:
-            fatorAtiv = 1.2
+    def generate_cal(dict: dict[str:float|str]) -> float:
+        """Computes and returns the user quantity of calories.
 
-        elif dict['nvlAtiv'] == 2:
-            fatorAtiv = 1.375
+        Args:
+            dict: A dictionary that contains the user data collected and passed
+            by the functions: validate_data and generate_dict. 
 
-        elif dict['nvlAtiv'] == 3:
-            fatorAtiv = 1.725
+        Returns:
+            A float value that represents the user quantity of calories.
+        """
+        match dict['nvlAtiv']:
+            case 1:
+                fator_ativ = 1.2
+            case 2:
+                fator_ativ = 1.375
+            case 3:
+                fator_ativ = 1.725
+            case _:
+                fator_ativ = 1.9
 
-        else:
-            fatorAtiv = 1.9
-
-        return round((dict['tmb'] * fatorAtiv), 2)
+        return round((dict['tmb'] * fator_ativ), 2)
 
     # adding the cal to data sent by the user
-    received['cal'] = generateCal(received)
+    received['cal'] = generate_cal(received)
 
-    def generateNutrients(dict):
-        carb = str(round((dict['cal'] * 0.45), 2))
-        prot = str(round((dict['cal'] * 0.3), 2))
-        fat = str(round((dict['cal'] * 0.25), 2))
+    def generate_nutrients(dict: dict[str:float|str]) -> dict[str: float]:
+        """Computes and returns the user quantity of nutrients.
+
+        Args:
+            dict: A dictionary that contains the user data collected and passed
+            by the functions: validate_data and generate_dict. 
+
+        Returns:
+            A dictionary that represents the user quantity of nutrientes.
+            The key is the name of the nutrient and the value its quantity
+            of calories.
+        """
+        carb_value = (dict['cal'] * 0.45)
+        prot_value = (dict['cal'] * 0.3)
+        fat_value  = (dict['cal'] * 0.25)
+        
+        carb = str(round(carb_value, 2))
+        prot = str(round(prot_value, 2))
+        fat =  str(round(fat_value, 2))
 
         return {"carboidratos": carb, "proteinas": prot, "gorduras": fat}
 
     # adding the nutrients to data sent by the user
-    received["nutrientes"] = generateNutrients(received)
-    print('O resultado do processamento é {}'.format(received))
+    received["nutrientes"] = generate_nutrients(received)
+    print(f'O resultado do processamento é {received}')
 
     # serialising
     result = json.dumps(received)
 
     # send a result
-    clientSocket.send(result.encode('ascii'))
+    client_socket.send(result.encode('ascii'))
     print('Os dados do cliente foram enviados com sucesso!')
 
     # finish a connection
-    clientSocket.close()
+    client_socket.close()
